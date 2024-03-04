1882e61bec728bc3aae784d31896e4d682516046
caesadaffa <caesadaffa@gmail.com>
2024-03-04 08:11:45 +0700

    feat : [CD] middleware

--- app/Http/Middleware/AuthenticatedMiddleware.php
+++ app/Http/Middleware/AuthenticatedMiddleware.php
@@ -2,28 +2,28 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class AuthenticatedMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, $guard = null)
+    public function handle(Request $request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            // If the user is authenticated, redirect to a different route
-            return redirect('/home'); // Change '/home' to the route you want to redirect to
+        // Check if the user is authenticated
+        if (Auth::check()) {
+            return $next($request); // User is authenticated, allow access
         }
 
-        return $next($request);
+        // User is not authenticated, redirect to login or perform any other action
+        return redirect()->route('LoginView');
     }
+
 }
